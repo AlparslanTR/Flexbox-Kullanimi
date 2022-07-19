6f8391c53c767addb0b47018c03b033b9b6e68fa
alparslan akbas <bybluestht@gmail.com>
2022-07-20 02:24:32 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CssMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Anasayfa()
+        {
+            return View();
+        }
+        public ActionResult Resimler()
+        {
+            return View();
+        }
+        public ActionResult FlexWebSayfası()
+        {
+            return View();
+        }
+    }
+}
